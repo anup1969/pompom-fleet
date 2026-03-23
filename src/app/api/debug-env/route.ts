d0457abeb463e1f720c40495d66fa2fc277dcc0c
anup1969 <anupn1969@gmail.com>
2026-03-23 10:05:12 +0530

    Remove debug endpoint

--- src/app/api/debug-env/route.ts
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-  const secret = process.env.FLEET_JWT_SECRET || '';
-  return Response.json({
-    has_secret: !!secret,
-    secret_length: secret.length,
-    secret_first_8: secret.substring(0, 8),
-    secret_last_8: secret.substring(secret.length - 8),
-  });
-}
